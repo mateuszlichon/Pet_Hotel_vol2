4777ff68d1338292aae9bea4be37e5655356a478
mateuszlichon <mateusz.lichon@gmail.com>
2018-04-09 15:33:57 +0200

    adds jpa config

---
+++ src/main/java/com/pethotel/config/JpaConfig.java
@@ -0,0 +1,54 @@
+package com.pethotel.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"com.pethotel.repository"})
+public class JpaConfig {
+
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "mysql.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder, DataSource dataSource, Environment env) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.pollapp.entity")
+                .properties(jpaProperties(env))
+                .build();
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        return new JpaTransactionManager();
+    }
+
+    private Map<String, Object> jpaProperties(Environment env) {
+        Map<String, Object> props = new HashMap<>();
+        props.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        return props;
+    }
+}
