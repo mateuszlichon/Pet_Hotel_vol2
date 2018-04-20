3724211c14c2d634dabda161bd3d6e552d24a82d
mateuszlichon <mateusz.lichon@gmail.com>
2018-04-20 20:00:31 +0200

    corrects config files

--- src/main/java/com/pethotel/config/ViewConfig.java
+++ src/main/java/com/pethotel/config/ViewConfig.java
@@ -1,14 +1,18 @@
 package com.pethotel.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+@EnableWebMvc
+@Configuration
 public class ViewConfig extends WebMvcConfigurerAdapter {
 
 
