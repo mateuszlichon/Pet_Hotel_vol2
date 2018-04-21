22efed984f99eabe5f3604edbd1261ce11838780
mateuszlichon <mateusz.lichon@gmail.com>
2018-04-21 14:32:10 +0200

    adds hotel entity

---
+++ src/main/java/com/pethotel/entity/Hotel.java
@@ -0,0 +1,53 @@
+package com.pethotel.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name = "hotel")
+public class Hotel {
+
+	@Id
+	private long id;
+	
+	@NotBlank
+	@Size(min = 5, max = 25)
+	private String name;
+	
+	@NotBlank
+	@Size(min = 5, max = 25)
+	private String description;
+
+	public Hotel() {
+		super();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
