98ebe7c5553fc6d6bc83a82d1ea3a7d7d27f27e6
mateuszlichon <mateusz.lichon@gmail.com>
2018-04-23 20:13:05 +0200

    establishes hotel list table

--- src/main/webapp/WEB-INF/resources/js/app.js
+++ src/main/webapp/WEB-INF/resources/js/app.js
@@ -1,3 +1,21 @@
 $(function () {
   var ajax = new Ajax();
-})
+  var hotelsList = $('.hotelsList');
+
+  function renderHotelList(endpoint) {
+    ajax.ajaxGetCallback(endpoint, function (response) {
+      // hotelsList.empty();
+      var data = response.content;
+      response.forEach(function (elem) {
+        $('.hotelsList').append('<tr class="table-active">' +
+        '<th scope="row">Active</th>' +
+        '<td>' + elem.name + '</td>' +
+        '<td>' + elem.description + '</td>' +
+        '</tr>');
+      })
+    })
+  }
+
+  renderHotelList('/hotels');
+
+});
