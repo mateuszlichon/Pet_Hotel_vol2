64ff751538168e44463ffa19918cdfcbb465dd81
mateuszlichon <mateusz.lichon@gmail.com>
2018-04-23 14:29:57 +0200

    sets up basic appjs and utiljs

--- src/main/java/com/pethotel/entity/Hotel.java
+++ src/main/java/com/pethotel/entity/Hotel.java
@@ -1,6 +1,8 @@
 package com.pethotel.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
@@ -12,6 +14,7 @@ import org.hibernate.validator.constraints.NotBlank;
 public class Hotel {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	@NotBlank
---
+++ src/main/webapp/WEB-INF/resources/js/app.js
@@ -0,0 +1,3 @@
+$(function () {
+  var ajax = new Ajax();
+})
